8900aeae9522c434bce717474c3cfd65dbad5f8a
MarkDv <markdevilliers@gmail.com>
2014-10-03 20:08:30 +0100

    print the sentinel ip's

--- provision_docker_redis_cluster.sh
+++ provision_docker_redis_cluster.sh
@@ -20,6 +20,15 @@ sudo docker run --name sentinel_0 -d -p 26379:26379 joshula/redis-sentinel --sen
 sudo docker run --name sentinel_1 -d -p 26378:26379 joshula/redis-sentinel --sentinel announce-ip $DOCKER_IP --sentinel announce-port 26378
 sudo docker run --name sentinel_2 -d -p 26377:26379 joshula/redis-sentinel --sentinel announce-ip $DOCKER_IP --sentinel announce-port 26377
 
+#get sentinel ips
+SENTINEL_0_IP=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' sentinel_0)
+SENTINEL_1_IP=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' sentinel_1)
+SENTINEL_2_IP=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' sentinel_2)
+
+echo "SENTINEL_0_IP : $SENTINEL_0_IP"
+echo "SENTINEL_1_IP : $SENTINEL_1_IP"
+echo "SENTINEL_2_IP : $SENTINEL_2_IP"
+
 redis-cli -h $REDIS_1_IP -p 6379 slaveof $REDIS_0_IP 6379
 
 redis-cli -p 26379 sentinel monitor testing $REDIS_1_IP 6379 2
